f3928dab56df3bff055cf7f8315dfbab10f2fa64
Stephanie <stephaniepui@outlook.com>
2016-01-09 15:18:13 -0800

    Assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,30 +2,17 @@
 
                 var points = document.getElementsByClassName('point');
 
-                var revealFirstPoint = function() {
-                    points[0].style.opacity = 1;
-                    points[0].style.transform = "scaleX(1) translateY(0)";
-                    points[0].style.msTransform = "scaleX(1) translateY(0)";
-                    points[0].style.WebkitTransform = "scameX(1) translateY(0)";
+                for (var i = 0; i < points.length; i++) {
+                  var revealPoint = function(i) {  
+                    points[i].style.opacity = 1;
+                    points[i].style.transform = "scaleX(1) translateY(0)";
+                    points[i].style.msTransform = "scaleX(1) translateY(0)";
+                    points[i].style.WebkitTransform = "scameX(1) translateY(0)";
+                   };
+                   revealPoint(i);
+                }
 
-                };
 
-                var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
+
 
             };
