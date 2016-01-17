c4180724ef2d353b13126605b749ebfee397625b
Stephanie <stephaniepui@outlook.com>
2016-01-17 10:18:52 -0800

    Checkpoint 29 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,18 +13,19 @@ var animatePoints = function(points){
     }
 };
 
-window.onload = function() {
-    //Automatically animate the points on a tall screen where scrolling can't trigger the animation
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+$(window).load(function() {
+    // #1
+    if ($(window).height() > 950) {
+        animatePoints();
     }
-
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    // #2
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    // #3
+    $(window).scroll(function(event) {
+        // #4
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
 
-}
+});
