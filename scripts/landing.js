4d32430ac803e8e1339876bc61b249db12f374b1
Stephanie <stephaniepui@outlook.com>
2016-01-20 22:42:56 -0800

    Assignment 32 WIP

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,14 @@
+
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points){
 
-    for (var i = 0; i < points.length; i++) {
+    for (var i = 0; i < pointsArray.length; i++) {
       var revealPoint = function(i) {  
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scameX(1) translateY(0)";
+        pointsArray[i].style.opacity = 1;
+        pointsArray[i].style.transform = "scaleX(1) translateY(0)";
+        pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
+        pointsArray[i].style.WebkitTransform = "scameX(1) translateY(0)";
         };
         revealPoint(i);
     }
